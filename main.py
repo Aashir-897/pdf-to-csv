33aa1706da3e6d8d4c5166c52ad9315e17fa635a
Aashir Ahmed <aashirahmed510@gmail.com>
2025-01-14 22:22:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+# pip install camelot-py
+# pip install gs
 import camelot
 
-
+# The path where you want to export pdf file
 tables = camelot.read_pdf('file:///C:/Users/Administrator/Downloads/foo.pdf')
 
 
 print(tables)
 
-# Export the first table to a CSV file
-tables[0].to_csv('foo.csv')  # or export it directly using tables.export()
+# Converting a Table pdf to csv
+tables[0].to_csv('foo.csv')  
 
-# Optionally, inspect the DataFrame
+
 df = tables[0].df
 print(df)
